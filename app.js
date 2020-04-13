7977acafcedb301b98f482e3422324e6199749a1
Obikaonu Samuel <samuelchibuike22@gmail.com>
2020-04-13 14:27:07 +0100

    added cors

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ const bodyParser = require("body-parser");
 const africaTalk = require("africastalking");
 const logger = require("morgan");
 const USSDRoute = require("./src/route/USSDRoute");
-
+const cors = require("cors");
 
 const app = express();
 
 // External middlewares
+app.use(cors())
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const port = process.env.port || "8080";
+app.set('port', (process.env.PORT || 8080));
 
 app.use("/", USSDRoute);
 
 app.get("/",(req,res)=>{
   res.status(200).json({success:true, message:"App Connected Successfully" })
 })
-app.listen(port, (err) => {
+app.listen(app.get('port'), (err) => {
   console.log("You are doing well");
 });
 
